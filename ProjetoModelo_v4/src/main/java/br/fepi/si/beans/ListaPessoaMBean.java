2cf49758472efde9cc3ef7c814ea800bbf887ef5
jean <jean@jean>
2020-05-23 13:33:18 -0300

    Versão

---
+++ ProjetoModelo_v4/src/main/java/br/fepi/si/beans/ListaPessoaMBean.java
@@ -0,0 +1,40 @@
+package br.fepi.si.beans;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.persistence.EntityManager;
+
+import br.fepi.si.model.Pessoa;
+import br.fepi.si.repository.Pessoas;
+import br.fepi.si.util.JpaUtil;
+
+@ManagedBean
+@ViewScoped
+public class ListaPessoaMBean implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	private List<Pessoa> pessoas = new ArrayList<>();
+	
+	public void consultar() {
+		EntityManager em = JpaUtil.getEntityManager();
+		this.pessoas = new Pessoas(em).todos();
+		em.close();
+	}
+
+	public List<Pessoa> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(List<Pessoa> pessoas) {
+		this.pessoas = pessoas;
+	}
+	
+	
+
+}
---
+++ ProjetoModelo_v5/src/main/java/br/fepi/si/repository/Lancamentos.java
@@ -0,0 +1,27 @@
+package br.fepi.si.repository;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
+import br.fepi.si.model.Lancamento;
+
+public class Lancamentos implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private EntityManager em;
+
+	public Lancamentos(EntityManager em) {
+		this.em = em;
+	}
+	
+	public List<Lancamento> todos(){
+		TypedQuery<Lancamento> query = 
+				em.createQuery("from Lancamento", Lancamento.class);
+		return query.getResultList();		
+	}
+
+}
